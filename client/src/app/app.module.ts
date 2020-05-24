4955ab021fc1edbdb6b193685a2ec337c862b211
SADIK112 <35666212+SADIK112@users.noreply.github.com>
2020-05-24 16:19:41 +0600

    updated

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignupComponent
+  ],
+  imports: [
+    FormsModule,
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ client/src/app/signup/signup.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+  signupUrl = "http://localhost:8000/auth/signup";
+
+  firstname: string;
+  lastname: string;
+  email: string;
+  password: string;
+  confirm_password: string;
+
+  constructor(public router: Router) { }
+
+  ngOnInit(): void { }
+
+  submitForm() {
+    let user = {
+      firstname: this.firstname,
+      lastname: this.lastname,
+      email: this.email,
+      password: this.password,
+    }
+    if (user.password == this.confirm_password) {
+      fetch(this.signupUrl, {
+        method: 'POST',
+        body: JSON.stringify(user),
+        headers: {
+          'content-type': "application/json"
+        }
+      }).then((response) => {
+        if (response.ok) {
+          return response.json()
+        }
+        return response.json().then((error) => {
+          throw new Error(error.message)
+        })
+      }).then(() => {
+        this.router.navigate(['/'])
+      }).catch((error) => {
+        console.log(error)
+      })
+    }
+    else {
+      return
+      alert("password does not match")
+    }
+  }
+
+}
